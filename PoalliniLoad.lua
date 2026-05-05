64ccd3f6d715e7b84781eb54ffd9b710cffb6a35
poallini <dhhdgdfedeifkf@gmail.com>
2026-05-05 15:07:03 -0300

    Update PoalliniLoad.lua

--- PoalliniLoad.lua
+++ PoalliniLoad.lua
@@ -1,3 +1,11 @@
 pcall(function()
     loadstring(game:HttpGet("https://raw.githubusercontent.com/poallini/loader.txt/refs/heads/main/poalliniloader.lua"))()
 end)
+
+print("Loaded Poallini Hub By poallini!")
+task.wait(0.1)
+
+warn("Loaded Poallini Hub By poallini!")
+task.wait(0.1)
+
+error("Loaded Poallini Hub By poallini!")
